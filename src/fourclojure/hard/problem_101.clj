efda7726c85eb4b76d31a193db468ce9d16dec57
Brad Lucas <brad@beaconhill.com>
2015-11-18 17:16:17 -0500

    added comment

--- src/fourclojure/hard/problem_101.clj
+++ src/fourclojure/hard/problem_101.clj
@@ -1,6 +1,7 @@
 (ns fourclojure.hard.problem-101)
 
 ;; Levenshtein Distance
+;; @see https://en.wikipedia.org/wiki/Levenshtein_distance
 
 ;; http://rosettacode.org/wiki/Levenshtein_distance#Clojure
 (defn levenshtein [str1 str2]
@@ -18,7 +19,8 @@
 
 ;; memoize to help speed things up
 ;; @see https://gist.github.com/laurentpetit/828413
-
+;; pass the memoized veresion of the function to itself
+;; and use that one to recurse to
 (defn prob101
   [a b]
   (let [levenshtein (fn [fnc str1 str2]
@@ -33,5 +35,3 @@
                                     (+ cost
                                        (fnc fnc (rest str1) (rest str2))))))))]
     (levenshtein (memoize levenshtein) a b)))
-
-;; 
