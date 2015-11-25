337af73c032dce493277367e2d9a13c6fd100438
Brad Lucas <brad@beaconhill.com>
2015-11-25 18:19:02 -0500

    working version of 138 not yet submitted

--- src/fourclojure/hard/problem_138.clj
+++ src/fourclojure/hard/problem_138.clj
@@ -16,7 +16,6 @@
 ;;                     " 6 4 "
 ;;                     "  1  "])
 
-
 ;; length of numbers needs to equal or pad out to the next number 
 ;; 1x1 2x2 3x3 4x4
 ;; 1     4   9  16  (256)
@@ -25,7 +24,6 @@
 ;; 1   3x3 5x5 7x7  (9x9)
 
 
- 
 ;; | Board |    | Snake |    |
 ;; |-------+----+-------+----|
 ;; |   1x1 |  1 |       |  1 |
@@ -46,27 +44,30 @@
                        (flatten (map digits nums))))]
     (digit-list 
      (loop [acc [start]]
-       (if (<= end (last acc))
+       (if (< end (last acc))
          (drop-last acc)
          (recur (conj acc (square (last acc)))))))))
 
-(defn pad [n coll val]
-  ;; @see http://stackoverflow.com/a/27263793 
-  (take n (concat coll (repeat val))))
-
-(defn empty-board [size] (vec (repeat size (vec (repeat size nil)))))
 
 ;; (defn fours [] (let [** (fn [x n] (reduce * (repeat n x)))]
 ;;                  (map #(** 4 %) (range))))
 
 (defn lengths [] 
+  ;; lengths of the strings
   ;; (1 4 9 16 25 ...)
+  ;; 
   (map #(* % %) (iterate inc 1)))
 
 (defn board-sizes []
   ;; (1 3 5 7 9 ...)
+  ;; or
+  ;; 1x1, 3x3, 5x5, 7x7, ....
+  ;; 1, 9, 25, 49
   (iterate #(+ % 2) 1))
 
+(defn pad [n coll val]
+  ;; @see http://stackoverflow.com/a/27263793 
+  (take n (concat coll (repeat val))))
 
 (defn pad-numbers
   [nums]
@@ -77,7 +78,10 @@
 
 (defn board-size 
   [nums]
-  (first (filter #(> (* % %) (count (pad-numbers nums))) (board-sizes))))
+  (let [padded-length (count (pad-numbers nums))]
+    (second (first (filter #(= (first %) padded-length) (map vector (lengths) (board-sizes)))))))
+
+(defn empty-board [size] (vec (repeat size (vec (repeat size nil)))))
 
 (defn blank-board [nums]
   (empty-board (board-size nums)))
@@ -87,23 +91,126 @@
   (vec (map (fn [x] (apply str (replace '{nil \space}  x))) xs)))
 
 ;; ----------------------------------------------------------------------------------------------------
+;; Snake Idea (11-25-2015)
+;; ----------------------------------------------------------------------------------------------------
+(defn start-position
+  [board]
+  ;; where should be start
+  ;; 1x1 board => [0 0]
+  ;; 3x3 board => [0 1]
+  ;; 5x5 board => [2 2]
+  ;; 7x7 board => [2 3]
+  (case (count board)
+    1 [0 0]
+    3 [0 1] ;;  [1 0] ;; [0 1]
+    5 [2 2] ;; [3 1] ;; [2 2]
+    7 [2 3]
+    nil   ;; TODO this function should be able to handle all sizes (formula)
+    )
+)
 
+(defn direction-value
+  [direction]
+  (
+   {:down-right [1 1]
+    :down-left  [1 -1]
+    :up-left    [-1 -1]
+    :up-right   [-1 1]
+    } direction)
+  )
 
+(defn turn-right
+  [direction]
+  ;; facing in the 'direction' return the point to the right
+  (case direction
+    :down-right :down-left
+    :down-left :up-left
+    :up-left :up-right
+    :up-right :down-right))
 
+(defn move-point
+  [board point new-direction]
+  ;; return the point in the 'new-direction'
+  ;; apply new-direction to point
+  (map + point (direction-value new-direction)))
+
+(defn look-right 
+  [board point direction]
+  ;; return the value to our right
+  (get-in board (move-point board point (turn-right direction))))
+
+(defn next-direction 
+  [board point direction]
+  ;; can we turn right?
+  (let [right-value (look-right board point direction)]
+    (if (nil? right-value)
+      (turn-right direction)
+      ;; else continue in the current-direction
+      direction)))
+
+(defn update-board
+  [board point value]
+  (assoc-in board point value))
+
+(defn snake
+  [board lst]
+  ;; write the lst into the board
+  ;; 1. find the starting point
+  (let [start-point (start-position board)]
+    ;; 2. write starting point and recurse over board moving through list
+    ;; - first move down-right
+    ;; - loop
+    ;; - can i turn right?
+    ;;   - turn and move
+    ;;   - continue
+    
+    (loop [board (update-board board start-point (first lst))
+           lst (rest lst)
+           current-point start-point
+           direction :up-right]
+      (println current-point " " direction)
+      (println board)
+      (if (empty? lst)
+        board
+        ;; next-point is right of 'point' or next in 'direction'
+        (let [new-direction (next-direction board current-point direction)]
+          (let [new-point (move-point board current-point new-direction)]
+            (println new-point " " new-direction)
+            (recur (update-board board new-point (first lst)) (rest lst) new-point new-direction)))))))
+
+;; ----------------------------------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------------------------------
 
 
 (defn prob138
   [start end]
-  (let [nums (squares-digit-list start end)
-        padded (pad-numbers nums)
-        board (blank-board nums)]
-    (println nums)
-    (println padded)
-    (println board)
-    ;; TODO write nums to the board
-    (write-board board)
-    ))
+  (let [digits (squares-digit-list start end)
+        board (empty-board (board-size digits))
+        lst (pad-numbers digits)
+        ]
+    (write-board (snake board lst))
+    )
+)
 
-
-
-
+(= (prob138 2 2) ["2"])
+(= (prob138 2 4) [" 2 "
+                  "* 4"
+                  " * "])
+(= (prob138 3 81) [" 3 "
+                   "1 9"
+                   " 8 "])
+(= (prob138 4 20) [" 4 "
+                   "* 1"
+                   " 6 "])
+(= (prob138 2 256) ["  6  "
+                    " 5 * "
+                    "2 2 *"
+                    " 6 4 "
+                    "  1  "])
+(= (prob138 10 10000) ["   0   "
+                       "  1 0  "
+                       " 0 1 0 "
+                       "* 0 0 0"
+                       " * 1 * "
+                       "  * *  "
+                       "   *   "])
