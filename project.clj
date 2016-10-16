726271a571e4b6fbf7a9243f289504ce3bfbe3d5
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:02:27 -0400

    version 1.8.0 of clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject fourclojure "0.1.0-SNAPSHOT"
+(defproject fourclojure "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main fourclojure.core
 )
